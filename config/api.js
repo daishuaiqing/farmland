9d81567f430948bc57a892895ac6fbd47967830f
代帅卿 <daishuaiqing@163.com>
2019-04-18 23:11:01 +0800

    完成首页功能

--- config/api.js
+++ config/api.js
@@ -1,10 +1,10 @@
 //本地开发
-var WxApiRoot = 'http://localhost/wx/';
+var WxApiRoot = 'http://192.168.0.103/wx/';
 //测试环境
 //生产环境
 module.exports = {
     WxApiRoot: WxApiRoot, //请求根目录
-    IndexCategoryUrl: 'home/index', //首页分类数据
-    IndexGalleryUrl: 'home/gallery',
-    WxLoginUrl: 'user/login',
+    IndexCategoryUrl: 'home/category', //首页分类数据
+    IndexGalleryUrl: 'home/gallery', //首页图片数据
+    WxLoginUrl: 'user/login', //微信登陆
 };
